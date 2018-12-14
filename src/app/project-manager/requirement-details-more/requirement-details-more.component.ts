648a81046404acd1d1e08408f2e6d9e9d99c20e0
lukjaw96 <lukasz.jaworski.96@gmail.com>
2018-12-14 01:22:53 +0100

    Chart JS working

--- src/app/project-manager/requirement-details-more/requirement-details-more.component.ts
+++ src/app/project-manager/requirement-details-more/requirement-details-more.component.ts
@@ -4,6 +4,7 @@ import { Estimation } from 'src/app/models/Estimation';
 import { ActivatedRoute } from '@angular/router';
 import { ProjectService } from 'src/app/services/project/project.service';
 import { RequirementService } from 'src/app/services/requirement/requirement.service';
+import * as Chart from 'chart.js'
 
 @Component({
   selector: 'app-requirement-details-more',
@@ -11,7 +12,7 @@ import { RequirementService } from 'src/app/services/requirement/requirement.ser
   styleUrls: ['./requirement-details-more.component.css']
 })
 export class RequirementDetailsMoreComponent implements OnInit {
-  
+
   idRequirement: string;
   requirement: Requirement = {
     idRequirement: null,
@@ -22,15 +23,51 @@ export class RequirementDetailsMoreComponent implements OnInit {
     finalCost: null
   };
   requirementEstimations: Estimation[] = [];
+  canvas: any;
+  ctx: any;
 
   constructor(
     private route: ActivatedRoute,
     private requirementService: RequirementService
   ) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+
     this.idRequirement = this.route.snapshot.paramMap.get('idRequirement');
-    this.requirementService.getRequirement(this.idRequirement).subscribe((result: { status: number, message: string, result: Requirement }) => {this.requirement = result.result; console.log("requirement", this.requirement)});
-    this.requirementService.getRequirementEstimations(this.idRequirement).subscribe((result: { status: number, message: string, result: Estimation[] }) => {this.requirementEstimations = result.result; console.log("estimations", this.requirementEstimations);});
+    this.requirementService.getRequirement(this.idRequirement).subscribe((result: { status: number, message: string, result: Requirement }) => { this.requirement = result.result; console.log("requirement", this.requirement) });
+    this.requirementService.getRequirementEstimations(this.idRequirement).subscribe((result: { status: number, message: string, result: Estimation[] }) => { 
+      this.requirementEstimations = result.result; 
+      console.log("estimations", this.requirementEstimations);
+    
+      this.canvas = document.getElementById('requirementsChartCanvas');
+      this.ctx = this.canvas.getContext('2d');
+  
+      let requirementsChart = new Chart(this.ctx, {
+        type: 'bar',
+        data: {
+          labels: ["e1", "e2", "e3"],
+          datasets: [{
+            label: 'requirements chart',
+            data: [
+              this.requirementEstimations[0].estimation,
+              this.requirementEstimations[1].estimation,
+              this.requirementEstimations[2].estimation
+            ],
+            backgroundColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          responsive: false
+        }
+      });
+    });
+  }
+
+  ngOnInit() {
   }
 }
